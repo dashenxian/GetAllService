22c22bf18314a8bf63b7359df9133b185e7f9013
1747623132@qq.com <1747623132@qq.com>
2020-12-21 11:48:11 +0800

    添加项目文件。

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GetAllService
+{
+    class Program
+    {
+        static void Main()
+        {
+            Host.CreateDefaultBuilder().ConfigureWebHostDefaults(builder => builder.UseStartup<Startup>())
+                .Build()
+                .Run();
+        }
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var provider = services.BuildServiceProvider();
+            foreach (var service in services)
+            {
+                var implementationType = service.ImplementationType
+                                         ?? service.ImplementationInstance?.GetType()
+                                         ?? service.ImplementationFactory?.Invoke(provider)?.GetType();
+                if (implementationType != null)
+                {
+                    Console.WriteLine($"{service.Lifetime,-15} {GetName(service.ServiceType),-50}{GetName(implementationType)}");
+                }
+            }
+        }
+        public void Configure(IApplicationBuilder app) { }
+        private string GetName(Type type)
+        {
+            if (!type.IsGenericType)
+            {
+                return type.Name;
+            }
+            var name = type.Name.Split('`')[0];
+            var args = type.GetGenericArguments().Select(it => it.Name);
+            return $"{name}<{string.Join(",", args)}>";
+        }
+    }
+}
